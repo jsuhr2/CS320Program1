cdb3100dd5d7e6638932f4e0e787c445ff81c5d4
jsuhr2 <jsuhr2@binghamton.edu>
2018-10-19 15:11:43 -0400

    wrote initial parsing code

--- predictors.cpp
+++ predictors.cpp
@@ -3,13 +3,17 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 
 using namespace std;
 
 int main(int argc, char** argv){
 	
-	
-	
+	string branch;
+	char takenOrNot;
+	int branchAddress;
+	int numTaken;
+	int numNotTaken;
 	ofstream clear;
 	clear.open(argv[2], ofstream::out | ofstream::trunc);
 	clear.close();
@@ -18,8 +22,15 @@ int main(int argc, char** argv){
 
 	ofstream output;
 	output.open(argv[2], ios_base::app);
-
 	
+	while(true){
+		input >> branch;
+		if(input.eof())
+			break;
+		takenOrNot = branch[0];
+		branch.erase(remove(branch.begin(), branch.end(), 'x'), branch.end());
+		branchAddress = strtol(branch.c_str(), NULL, 16); 
+	}
 	
 	input.close();
 	output.close();
